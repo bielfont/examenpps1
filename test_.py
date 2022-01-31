71d2dae38077bd75a2ce986286011eb80812b45b
Biel Font <biel.font@inphower.com>
2022-01-31 19:24:47 +0100

    99

--- test_.py
+++ test_.py
@@ -3,5 +3,5 @@ import alumnes
 
 
 def test1():
-    assert alumnes.orden2([{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]) == "[{'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}, {'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]"
+    assert alumnes.orden2([{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]) == "[{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]"
 
