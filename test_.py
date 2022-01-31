4c27f374e958af82ec1473f1e513c1bfc7f4cee7
Biel Font <biel.font@inphower.com>
2022-01-31 19:18:26 +0100

    con lista en pytest

--- test_.py
+++ test_.py
@@ -1,9 +1,10 @@
 import alumnes
 
 
+
 def test1():
-    assert alumnes.orden2() == "[{'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}, {'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]"
+    assert alumnes.orden2([{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]) == "[{'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}, {'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]"
 
 
 def test2():
-    assert alumnes.orden1() == "[{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}]"
+    assert alumnes.orden1([{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]) == "[{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}]"
