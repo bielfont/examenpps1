a21a90ceb63385db1387c7290aa2365b9cc06243
Biel Font <biel.font@inphower.com>
2022-01-31 19:18:14 +0100

    con lista en el pytest

--- alumnes.py
+++ alumnes.py
@@ -1,15 +1,11 @@
-lista = [{'nombre': 'Alba', 'fecha_nac': '1996-03-02'},
-         {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'},
-         {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}]
 
-
-def orden1():
+def orden1(lista):
     lista.sort(key=lambda x: x['nombre'])
     # print("Ordenado por fecha nombre", str(lista))
     return str(lista)
 
 
-def orden2():
+def orden2(lista):
     lista.sort(key=lambda x: x['fecha_nac'])
     # print("Ordenado por fecha nacimiento", str(lista))
     return str(lista)
