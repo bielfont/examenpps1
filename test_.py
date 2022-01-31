1e5b4ee92309b9f61b2391a98f080871488ce6cc
Biel Font <biel.font@inphower.com>
2022-01-31 17:57:51 +0100

    confallos

--- test_.py
+++ test_.py
@@ -1,8 +1,8 @@
 import alumnes
 
 
-#def test():
-#    assert alumnes.orden1() == "Ordenado por fecha nombre [{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}]"
-
 def test():
-    assert alumnes.orden3() == None
+    assert alumnes.orden1() == "Ordenado por fecha nombre [{'nombre': 'Alba', 'fecha_nac': '1996-03-02'}, {'nombre': 'Borja', 'fecha_nac': '1997-09-13'}, {'nombre': 'Vicente', 'fecha_nac': '1995-01-04'}]"
+
+#def test():
+#    assert alumnes.orden3() == None
