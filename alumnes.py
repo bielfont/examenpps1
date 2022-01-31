92d30802eb226c57c491383146c355e2f8dfe887
Biel Font <biel.font@inphower.com>
2022-01-31 19:36:36 +0100

    999

--- alumnes.py
+++ alumnes.py
@@ -1,4 +1,7 @@
 
+from datetime import datetime
+
+
 def orden1(lista):
     lista.sort(key=lambda x: x['nombre'])
     # print("Ordenado por fecha nombre", str(lista))
@@ -8,6 +11,9 @@ def orden1(lista):
 def orden2(lista):
     lista.sort(key=lambda x: x['fecha_nac'])
     # print("Ordenado por fecha nacimiento", str(lista))
+    #def naix(element):
+    #  return datetime.strptime(element['fecha_nac','%y-%m-%d'])
+    #  lista.sort(key=naix)
     return str(lista)
 
 
