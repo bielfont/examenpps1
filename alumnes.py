9f1f1f769c56dafd337b9fbcef99d8d066c8c105
Biel Font <biel.font@inphower.com>
2022-01-31 16:44:30 +0100

    Fase1

---
+++ alumnes.py
@@ -0,0 +1,11 @@
+
+students = [
+  {'alumne': 'Pepe Botella', 'data_naiximent': '10-02-2005'},
+  {'alumne': 'Rafa Nadal', 'data_naiximent': '09-02-2005'},
+  {'alumne': 'Pedro Pasqua', 'data_naiximent': '10-02-2003'},
+  {'alumne': 'Manolo Mata', 'data_naiximent': '09-02-2008'},
+  {'alumne': 'Juan Gris', 'data_naiximent': '10-02-2000'},
+  {'alumne': 'Maria Debe', 'data_naiximent': '10-02-1998'}
+]
+
+print(students)
